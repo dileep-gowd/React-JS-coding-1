d6c0dd050eae9592963236398ee1f5cacf530c87
dileep-gowd <srisailamdileep22@gmail.com>
2023-04-24 15:14:27 +0000

    React JS coding

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="bg-cont">
+    <h1 className="head1">Super Over League</h1>
+    <div className="img-cont">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="img1"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="img1"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
